5b9d3520497f7c885ea9dd1f1c5b4a7260a577ef
FIRAT TALE <talefirat@gmail.com>
2019-06-30 14:36:52 +0300

    Creating Board component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 // import Login from "../Login/Login";
-import Card from "../Card";
-
+import Board from "../Board";
+import initializeDeck from "../../utils";
 export default function App() {
+  const [cards, setCards] = useState([]);
   const [flipped, setFlipped] = useState([]);
+  useEffect(() => {
+    setCards(initializeDeck());
+  }, []);
   const handleClick = id => {
-    setFlipped(id);
+    setFlipped([...flipped, id]);
   };
 
   return (
@@ -14,15 +18,7 @@ export default function App() {
       <h1>Memory Game</h1>
       <div>
         {/* <Login loginVisibility={loginVisibility} /> */}
-        <Card
-          id={1}
-          width={100}
-          height={100}
-          back={"/img/back.png"}
-          front={"/img/react.png"}
-          flipped={flipped.includes(1)}
-          handleClick={() => handleClick(1)}
-        />
+        <Board cards={cards} flipped={flipped} handleClick={handleClick} />
       </div>
     </div>
   );
---
+++ src/utils/index.js
@@ -0,0 +1,25 @@
+export default function initializeDeck() {
+  let id = 0;
+  const cards = [
+    "react",
+    "reactu",
+    "redux",
+    "vue",
+    "angular",
+    "javascript",
+    "ruby",
+    "rails"
+  ].reduce((acc, type) => {
+    acc.push({
+      id: id++,
+      type
+    });
+    acc.push({
+      id: id++,
+      type
+    });
+    return acc;
+  }, []);
+
+  return cards;
+}
