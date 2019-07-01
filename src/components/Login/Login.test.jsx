4da6d4024c778ee2a8029a816198da355e53a4b4
FIRAT TALE <talefirat@gmail.com>
2019-07-01 21:31:02 +0300

    Adding tests

--- src/components/Login/Login.test.jsx
+++ src/components/Login/Login.test.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Login from "./index";
-
-it("should render correctly", () => {
+import { Login } from ".";
+it("renders without crashing", () => {
   const wrapper = shallow(<Login />);
   expect(wrapper).toMatchSnapshot();
 });
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-function shuffle(array) {
+export function shuffle(array) {
   const copyArray = array.slice(0);
   for (let i = 0; i < array.length; i++) {
     let randomIndex = Math.floor(Math.random() * (i + 1));
